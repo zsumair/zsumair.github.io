d05d644d866dae71b98e6146c5a478b88fbf76f4
syedzoheb1242@gmail.com <syedzoheb1242@gmail.com>
2019-10-21 22:17:32 +0530

    Updates

---
+++ component---src-pages-work-js-59198d8195764a3dd527.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{221:function(e,n,a){"use strict";a.r(n);var t=a(0),o=a.n(t),r=a(218),l=a(217);n.default=function(){return o.a.createElement(r.a,null,o.a.createElement(l.a,{title:"Work"}),o.a.createElement("h4",{className:"heading"},"There's gonna be an awesome lot of work to be featured here, hang tight."),o.a.createElement("h2",null,"Coming Soon!!"))}}}]);
+//# sourceMappingURL=component---src-pages-work-js-59198d8195764a3dd527.js.map
